8a75fcd36666f22f68f10398fb12a07356d4f8ec
Nygus <witekszymkowiak193@gmail.com>
2025-09-17 13:20:07 +0200

    Add run_demo.py as entrypoint for demo

---
+++ run_demo.py
@@ -0,0 +1,10 @@
+﻿from test_tracker import MainProgram
+
+program = MainProgram(
+    model_path="assets/models/best.xml",
+    video_path="assets/videos/demo.mp4",
+    categorizer_threshold=10000,
+    use_redis=False
+)
+
+program.start_processing()
